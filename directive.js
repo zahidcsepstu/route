f64ac0fab8c2220ff490a9083625a6bb5b634cd7
zahid <zahid13@cse.pstu.ac.bd>
2020-08-06 18:06:59 +0600

    2NDCOMMIT

--- directive.js
+++ directive.js
@@ -1,17 +1,16 @@
-(function(){
+(function () {
   "use strict"
-    const app = angular.module("testRoute");
-    app.controller("newDirectiveCtrl", newDirectiveCtrl);
-  function newDirectiveCtrl($scope){
-    $scope.view = "asdadas";
+  const app = angular.module("testRoute");
+  app.controller("newDirectiveCtrl", newDirectiveCtrl);
+  function newDirectiveCtrl($scope) {
+    $scope.view = "zAHID";
   }
-  app.directive("newDirective",newDirective);
-  function newDirective(){
-    debugger
+  app.directive("newDirective", newDirective);
+  function newDirective() {
     return {
-     controller: "newDirectiveCtrl",
-     templateUrl: 'directive.html'
-   };
+      controller: "newDirectiveCtrl",
+      templateUrl: 'directive.html'
+    };
   }
 
 })();
