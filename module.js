87bed0601548303f4db4f9fbf48e206d38c5852b
zahid <zahid13@cse.pstu.ac.bd>
2020-08-08 17:45:05 +0600

    latest version added

--- module.js
+++ module.js
@@ -1,6 +1,6 @@
 (function () {
   "use strict"
-  var app = angular.module("testRoute", ["ngRoute"]);
+  var app = angular.module("testRoute", ["ngRoute", "ngMaterial"]);
 
   app.config(function ($routeProvider) {
     $routeProvider
